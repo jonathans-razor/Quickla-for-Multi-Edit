4ae7eafd108e5ee7a003693defab7844ddc4b6ad
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-18 17:43:37 -0500

    Feb-18-2024-5-43-37-PM

--- Jonathan's_Macros.s
+++ Jonathan's_Macros.s
@@ -8259,4 +8259,64 @@ rs = '\0';
 
 
 
+//;
+
+void
+@xrtm
+{
+str fp = "Find illegal z references.";
+
+// lu: Feb-18-2024
+
+str rs;
+str sc;
+
+@header;
+sc = '!1z';
+@eol;
+
+int is_found = @seek_in_all_files_2_arguments(sc, fp);
+return();
+rs = '\0';
+@seek(sc);
+@replace_next_occurrence_only(sc, rs);
+@replace_all_occurrs_inf_one_tof(sc, rs);
+
+@footer;
+@say(found_str);
+@say(fp);
+}
+
+
+
+//;
+
+void
+@rtm
+{
+str fp = "sanction duplicate lks.";
+
+// lu: Feb-18-2024
+
+str rs;
+str sc;
+
+@header;
+sc = 'sanctioned duplicate entry: lk\#2';
+@eol;
+
+int is_found = @seek_in_all_files_2_arguments(sc, fp);
+return();
+@seek(sc);
+rs = '\0';
+@replace_next_occurrence_only(sc, rs);
+@replace_all_occurrs_inf_one_tof(sc, rs);
+
+@footer;
+@say(found_str);
+@say(fp);
+}
+
+
+
 //; (!efjm)
